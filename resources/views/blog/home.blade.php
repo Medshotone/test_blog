887323e3e5f509e01b379564a7b6efdd62920254
Medshot <chornogovlada@gmail.com>
2019-11-17 20:45:04 +0200

    add home views

---
+++ resources/views/blog/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', "Laravel-Blog")
+
+@section('content')
+    @foreach ($categories as $category)
+        <a href="{{ route('category', $category->slug) }}" class="list-group-item">
+            <h4 class="list-group-item-heading">{{ $category->title }}</h4>
+            <p class="list-group-item-text">
+                Количество постов : {{$category->articles()->count()}}
+            </p>
+        </a>
+    @endforeach
+
+
+@endsection
