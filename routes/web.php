f5b33a3a8838ed9d8a1907e88ec2df3de3d7b91a
Medshot <chornogovlada@gmail.com>
2019-11-17 19:05:37 +0200

    routes add category + article

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function(){
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+    Route::resource('/category', 'CategoryController', ['as'=>'admin']);
+    Route::resource('/article', 'ArticleController', ['as'=>'admin']);
 });
 
 Auth::routes();
