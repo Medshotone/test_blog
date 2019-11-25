fc63c693887317c1275c0515bb6e138e5aec36ee
Medshot <chornogovlada@gmail.com>
2019-11-25 23:41:00 +0200

    remove jenssegers/agent from service provider

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
 
         // Our packages
         Unisharp\Ckeditor\ServiceProvider::class,
-        Jenssegers\Agent\AgentServiceProvider::class,
 
     ],
 
@@ -229,7 +228,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Agent' => Jenssegers\Agent\Facades\Agent::class,
 
     ],
 
