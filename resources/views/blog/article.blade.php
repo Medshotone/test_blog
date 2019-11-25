5a31e3a21346f73386cee6d89519819db4419efc
Medshot <chornogovlada@gmail.com>
2019-11-25 21:51:30 +0200

    ajax load comment

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -13,9 +13,11 @@
     </div>
     <div class="col-sm-12">
       <h4>Display Comments</h4>
-      <div id="article-comments"></div>
+      <div id="article-comments">No comments</div>
       <script type="application/javascript">
         //comment ajax load
+
+        document.addEventListener("DOMContentLoaded", function(event) {
           $.ajax({
             headers: {
               'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -24,26 +26,17 @@
             type: 'GET',
             dataType: 'json',
             success: function (json) {
-              let html = '<div class="list-group">';
+              let html = '';
               for(let key in json) {
-                html += '<div class="list-group-item list-group-item-action flex-column align-items-start">';
 
-                html += '<div class="d-flex w-100 justify-content-between">';
-                html += "<h5 class='mb-1'>Comment</h5>";
-                html += '<small>'+json[key]['created_at']+'</small>';
-                html += '</div>';
+                html += comment.template(json[key]);
 
-                html += '<p class="mb-1">'+json[key]['body']+'</p>';
-
-                html += '<small>'+json[key]['user_name']+'</small>';
-
-                html += '</div>';
               }
-              html += '</div>';
 
               $('#article-comments').html(html);
             }
           });
+        });
       </script>
       <hr />
       <h4>Add comment</h4>
@@ -62,39 +55,6 @@
           <input type="submit" class="btn btn-warning" value="Add Comment" />
         </div>
       </form>
-      <script type="application/javascript">
-        $('form#article-comment').submit(function (e) {
-          e.preventDefault();
-          $.ajax({
-            url: '{{ route("comment.store") }}',
-            data:$(this).serialize(),
-            type: 'POST',
-            dataType: 'json',
-            success: function (json) {
-              let html = '<div class="list-group">';
-
-                html += '<div class="list-group-item list-group-item-action flex-column align-items-start">';
-
-                html += '<div class="d-flex w-100 justify-content-between">';
-                html += "<h5 class='mb-1'>Comment</h5>";
-                html += '<small>'+json.comment['created_at']+'</small>';
-                html += '</div>';
-
-                html += '<p class="mb-1">'+json.comment['body']+'</p>';
-
-                html += '<small>'+json.comment['user_name']+'</small>';
-
-                html += '</div>';
-
-              html += '</div>';
-
-              $('#article-comments').append(html);
-
-              e.currentTarget.reset();
-            }
-          });
-        });
-      </script>
     </div>
   </div>
 </div>
