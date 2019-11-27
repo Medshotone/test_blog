e7d64a2794b4764e1a77a6cf1f4f6fc5e65afea0
Medshot <chornogovlada@gmail.com>
2019-11-27 09:09:50 +0200

    browser counter

---
+++ app/Browser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Browser extends Model
+{
+    //Mass assigned
+    protected $fillable = ['name', 'count'];
+
+}
--- app/Providers/BrowserServiceProvider.php
+++ app/Providers/BrowserServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Jenssegers\Agent\Agent;
+use App\Browser;
 
 class BrowserServiceProvider extends ServiceProvider
 {
@@ -14,9 +15,8 @@ class BrowserServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $agent = new Agent();
-        $browser = $agent->browser();
-        var_dump($browser);
+
+        //
     }
 
     /**
@@ -26,6 +26,27 @@ class BrowserServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        //get browser name
+        $agent = new Agent();
+        $browser_name = $agent->browser();
+
+        //init browser model
+        $browser = new Browser;
+
+        //check if browser exist
+        $new_browser =  !(bool)$browser->where('name', $browser_name)->count();
+
+        if ($new_browser){
+            //create new
+            $browser->name = $browser_name;
+
+            $browser->count = 1;
+            $browser->save();
+        }else{
+            //update count
+            $browser->where('name', $browser_name)->increment('count');
+        }
+
     }
 }
